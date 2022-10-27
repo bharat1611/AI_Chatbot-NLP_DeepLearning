099574f2a8fdbcb8f9623beaf1cdaf5fc6284034
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-10-28 01:29:20 +0530

    Updating Training

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,3 +1,6 @@
+import token
+from xmlrpc.client import _datetime_type
+import numpy as np
 import nltk
 #nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer 
@@ -6,17 +9,30 @@ stemmer = PorterStemmer()
 def tokenize(sentence):
     return nltk.word_tokenize(sentence);
 
-def stem(word):
-    return stemmer.stem(word.lower())
-
-def bag_of_words(tokenized_sentence, all_words):
-    pass
-
 # a = ["Which items do you have?", "yo"]
 # print(a)
 # a = tokenize(a)
 # print(a)
 
-words = ["this", "that", "three"]
-stemmed_words = [stem(w) for w in words]
-print(stemmed_words)
+def stem(word):
+    return stemmer.stem(word.lower())
+
+# words = ["this", "that", "three"]
+# stemmed_words = [stem(w) for w in words]
+# print(stemmed_words)
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+
+    bag = np.zeros(len(all_words), dtype = np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
+
+# sentence = ["hello", "how", "are", "you"]
+# words = ["hi", "hello","i", "you", "bye", "thank", "cool"]
+# bag = bag_of_words(sentence, words)
+# print(bag)
--- train.py
+++ train.py
@@ -1,16 +1,48 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Datasset, Dataloader
+
+
 with open('intents.json', 'r') as f:
     intents = json.load(f);
 
+# Tokenize
 all_words = []
 tags = []
 xy = []
 for intent in intents['intents']:
     tag = intent['tag']
     tags.append(tag)
-    for pattern in intents['patterns']:
+    for pattern in intent['patterns']:
         w = tokenize(pattern)
         all_words.extend(w)
         xy.append((w, tag))
+
+# Lowercase + Stemming + Ignore Punctuations:
+ignore_words = ['?', '!', '.',',']
+# print(all_words)
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# print(all_words)
+# sorting and removing duplicates :  
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+# print(all_words)
+# print(tags)
+
+# Creating Bag of words and Training Data
+X_train = []
+Y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tags)
+    # Cross Entropy Loss
+    Y_train.append(label) 
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
 
