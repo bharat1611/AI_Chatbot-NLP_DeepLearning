836603ba6e32f749f775ee13a43cedee4f07f658
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-10-28 01:38:26 +0530

    Implementing Pytorch Dataset

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import Datasset, Dataloader
+from torch.utils.data import Dataset, DataLoader
 
 
 with open('intents.json', 'r') as f:
@@ -46,3 +46,26 @@ for (pattern_sentence, tag) in xy:
 X_train = np.array(X_train)
 Y_train = np.array(Y_train)
 
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+    # dataset[idx] :
+    def __getitem__(self, idx):
+        return self.x_data[idx], self.y_data[idx]
+    
+    def __len__(self):
+        return self.n_samples
+
+
+# Hyperparameters
+batch_size = 8
+dataset = ChatDataset()
+train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
+
+
+    
+
+    
